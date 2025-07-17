dde09fb7dc8d1b7cfb3b93b02ab2fc18f6d424fb
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-07-17 19:50:12 +0300

    modify()

--- Matrix.cpp
+++ Matrix.cpp
@@ -37,8 +37,26 @@ TElem Matrix::element(int i, int j) const {
 }
 
 TElem Matrix::modify(int i, int j, TElem e) {
-    //TODO - Implementation
-    return NULL_TELEM;
+    TElem oldElem;
+
+    if (i < 0 || j < 0 || i >= nrOfRows || j >= nrOfCols) {
+        throw runtime_error("Wrong input");
+    }
+    if (nrOfRows <= 0 || nrOfCols <= 0) {
+        throw runtime_error("Wrong input");
+    }
+
+    for (int index = 0; index <= nrOfCols - 1; index++) {
+        if (valueRow[index] == i && valueCol[index] == j) {
+            oldElem = elem[index];
+
+            if (e == NULL_TELEM) {
+                //functie de sters x[index]
+            }
+            valueRow[index] = e;
+            return oldElem;
+        }
+    }
 }
 
 Matrix::~Matrix() {
